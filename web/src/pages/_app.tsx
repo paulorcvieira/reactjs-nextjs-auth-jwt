51aec7999ffb83b63af1555d7c615feb0b7fd980
Paulo Vieira <paullocostta86@gmail.com>
2021-05-10 21:26:29 -0300

    feat: authentication

---
+++ web/src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import { AuthProvider } from '../contexts/AuthContext'
+
+import '../styles/globals.css'
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
+}
